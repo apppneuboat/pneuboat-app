e5896f2e09cb5269c8516cb663e0d7f9c5f819a0
apppneuboat <youousek@gmail.com>
2026-02-11 22:05:00 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 40 }}>
+      <h1>Pneuboat - Application de Facturation</h1>
+      <p>Application en cours de développement 🚀</p>
+      <p>Gérant : Sekkal Gherbi Youcef</p>
+    </div>
+  );
+}
